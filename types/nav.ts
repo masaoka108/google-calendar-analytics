a900c5f8b313dd001049ca56462913e371c75ae3
masahiro okamura <>
2024-12-28 21:44:25 +0900

    vercelエラーに対応

--- types/nav.ts
+++ types/nav.ts
@@ -1,6 +1,17 @@
+import { LucideIcon } from "lucide-react"
+
 export interface NavItem {
   title: string
   href?: string
   disabled?: boolean
   external?: boolean
+  icon?: LucideIcon
 }
+
+export interface NavItemWithChildren extends NavItem {
+  items: NavItem[]
+}
+
+export interface MainNavItem extends NavItem {}
+
+export interface SidebarNavItem extends NavItemWithChildren {}
